dd0700fc091c357f3afab0be448790b1a04f7bc4
Jorge Arellanes <jo2409@hotmail.com>
2014-04-06 12:52:18 -0500

    Resolviendo conflicto de la imagen

--- web/app/webroot/js/estilos.js
+++ web/app/webroot/js/estilos.js
@@ -6,5 +6,12 @@ $(function() {
 		$('div.input input').css('width',235);
 		$('div.input textarea').css('width',235);
 		$('div.input select').css('width',70);	
+		$('#logo').css('border-bottom-width', 0);
+		$('#logo').css('bottom', -27);
+		$('#logo').css('height', 72);
+		$('#logo').css('left', 21);
+		$('#logo').css('margin', 0);
+		$('#logo').css('padding', 0);
+		$('#logo').css('width', 425);
 	});
 });
